75b580feccebdf9319f06af5a774681f6fa2f778
ShravanEssence <shravankumar.essence@gmail.com>
2023-05-24 11:07:25 +0530

    Added rubocop and enabled mfa in gemspec file

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "reverse_string/version"
-
+# this ReverseString module is used for reversing the string.
 module ReverseString
   def self.reverse(str)
     str.reverse
