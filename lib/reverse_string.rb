b6c404c23c7fda22344f74eade992df7c2a34363
ShravanEssence <shravankumar.essence@gmail.com>
2023-05-24 10:51:09 +0530

    Reverse String gem

---
+++ lib/reverse_string.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "reverse_string/version"
+
+module ReverseString
+  def self.reverse(str)
+    str.reverse
+  end
+end
---
+++ spec/reverse_string_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ReverseString do
+  it "has a version number" do
+    expect(ReverseString::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
